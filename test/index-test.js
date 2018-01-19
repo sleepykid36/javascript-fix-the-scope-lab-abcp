bd9b6ab9c413993975ffbcea632cfcd5016f2a05
Shaun Tung <shaun.tung@gmail.com>
2018-01-19 17:25:00 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*global describe, it */
 
 describe('Fix the Scope', function() {
@@ -37,3 +38,44 @@ describe('Fix the Scope', function() {
   })
 
 })
+=======
+/*global describe, it */
+
+describe('Fix the Scope', function() {
+  describe('myAnimal()', function() {
+    it('returns my animal', () => {
+      expect(window.myAnimal()).toEqual('dog')
+    })
+  })
+
+  describe('yourAnimal()', function() {
+    it('returns your animal', function(){
+      expect(window.yourAnimal()).toEqual('cat')
+    })
+
+    it('does not hard-code the answer', function() {
+      expect(window.yourAnimal.toString()).("retutoNotContainrn 'cat'")
+    })
+  })
+
+  describe('add2(n)', function() {
+    it('adds two to n', function() {
+      const n = Math.floor(Math.random() * 1000)
+      expect(window.add2(n)).toEqual(n + 2)
+    })
+  })
+
+  describe('funkyFunction()', function() {
+    it('returns a function', function() {
+      expect(typeof window.funkyFunction()).toEqual('function')
+    })
+  })
+
+  describe('theFunk', function() {
+    it('is "FUNKY!"', function() {
+      expect(window.theFunk).toEqual('FUNKY!')
+    })
+  })
+
+})
+>>>>>>> 29b7801087753bf1911bcdb0c44967fe732849d7
